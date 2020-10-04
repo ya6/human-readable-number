5c5ea134c3fc423882846358d5d967c18f698fd8
YA <vector_cp@mail.ru>
2020-10-04 11:37:28 +0300

    init: make task!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 module.exports = function toReadable (number) {
-  
+    const obj = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+        100: 'hundred',
+    };
+
+
+    let num = '';
+    if ((number) === 100) {
+        num = obj[1] + ' ' + obj[100];
+        return num;
+    }
+
+    if ((number) / 100 > 1) {
+        num += obj[Math.trunc(number / 100)] + ' ' + obj[100];
+        number = number - Math.trunc(number / 100) * 100;
+    }
+
+    if (number > 0 && number < 20) {
+        num += ' ' + obj[number];
+        return num.trim();
+
+    }
+    if ((number) / 10 > 1) {
+        num += ' ' + obj[Math.trunc(number / 10) * 10];
+        number = number - Math.trunc(number / 10) * 10;
+    }
+
+    if (number > 0) {
+        num += ' ' + obj[number];
+    }
+
+    if (num === '' && number === 0) {
+        return obj[0]
+    };
+
+    return num.trim();
 }
